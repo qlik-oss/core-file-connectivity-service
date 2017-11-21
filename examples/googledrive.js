2d6ad82a4964dace85c58bc7ad33d7ea1cac1f27
Stefan Enberg <Stefan.Enberg@qlik.com>
2017-11-21 13:23:02 +0100

    Fixed missing part of path

--- examples/googledrive.js
+++ examples/googledrive.js
@@ -1,8 +1,8 @@
 const Outhaul = require('../src/outhaul.js');
 const request = require('supertest');
 
-const GoogleDriveStrategy = require('./googledrive/googledrive.js');
-const OneDriveStrategy = require('./onedrive/onedrive.js');
+const GoogleDriveStrategy = require('../strategies/googledrive/googledrive.js');
+const OneDriveStrategy = require('../strategies/onedrive/onedrive.js');
 
 async function run() {
   const GoogleDrive = new GoogleDriveStrategy('811557351071-2q71bjutd6fnppg24ps5nposmk42e97t.apps.googleusercontent.com', 'yi4C3WagMm4J2Ig2Vr4xYbSZ');
