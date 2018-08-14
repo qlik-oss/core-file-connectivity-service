958234cccff5eedc96cfa08530b97620bb6fce7a
Jan Miderbäck <jan.miderback@qlik.com>
2018-08-14 14:34:24 +0200

    Using load script directly instead of halyard.js

--- examples/dropbox.js
+++ examples/dropbox.js
@@ -3,19 +3,8 @@ const WebSocket = require('ws');
 const schema = require('enigma.js/schemas/12.20.0.json');
 const enigma = require('enigma.js');
 const open = require('open');
-const Halyard = require('halyard.js');
-const enigmaMixin = require('halyard.js/dist/halyard-enigma-mixin.js');
 
-const host = 'localhost';
-
-// create a new session:
-const session = enigma.create({
-  schema,
-  url: `ws://${host}:19076/app/engineData`,
-  createSocket: url => new WebSocket(url),
-  mixins: enigmaMixin,
-});
-
+// Authentication helper function
 async function waitForAuthentication(uri) {
   let resolve;
   let reject;
@@ -39,62 +28,62 @@ async function waitForAuthentication(uri) {
   };
 
   setTimeout(pullFn, 0);
-
   return pullPromise;
 }
 
-async function e2e() {
-  const addConnectionResult = await request.post('http://localhost:3000/v1/connections/').send({
-    connector: 'Dropbox',
-    params: {
-      filePath: '/airports.csv',
-    },
-  });
-
-  const localhostAccessibleURL = `http://localhost:3000/v1${addConnectionResult.text}`;
-
-  // Initiate the OAuth 2.0 authentication flow
-  open(`${localhostAccessibleURL}/authentication`);
-
-  await waitForAuthentication(localhostAccessibleURL);
-
-  const halyard = new Halyard();
-
-  const dockerNetworkAccessibleURL = `http://file-connectivity-service:3000/v1${addConnectionResult.text}`;
-
-  halyard.addTable(
-    new Halyard.Connections.Web(dockerNetworkAccessibleURL, 'csv'),
-    {
-      name: 'Airports',
-      characterSet: 'utf8',
-      headerRowNr: 1,
-    },
-  );
-
-  const global = await session.open();
-
-  const appName = 'Airports';
-
+(async () => {
   try {
-    const app = await global.reloadAppUsingHalyard(appName, halyard, true);
-    console.log(`App created and reloaded - ${appName}.qvf`);
+    console.log('Adding Dropbox connection in file-connectivity-service to airports.csv file');
+    const addConnectionResult = await request.post('http://localhost:3000/v1/connections/').send({
+      connector: 'Dropbox',
+      params: {
+        filePath: '/airports.csv',
+      },
+    });
 
+    const localhostAccessibleURL = `http://localhost:3000/v1${addConnectionResult.text}`;
+    const dockerNetworkAccessibleURL = `http://file-connectivity-service:3000/v1${addConnectionResult.text}`;
+
+    console.log('Initiating OAuth 2.0 authentication flow in file-connectivity-service');
+    open(`${localhostAccessibleURL}/authentication`);
+    await waitForAuthentication(localhostAccessibleURL);
+
+    console.log('Creating and opening engine session');
+    const session = enigma.create({
+      schema,
+      url: 'ws://localhost:19076',
+      createSocket: url => new WebSocket(url),
+    });
+
+    const global = await session.open();
+    const app = await global.createSessionApp();
+
+    console.log('Creating engine web file connection');
+    await app.createConnection({
+      qName: 'data',
+      qConnectionString: dockerNetworkAccessibleURL,
+      qType: 'internet',
+    });
+
+    console.log('Executing engine load script');
+    const loadScript = `Airports:
+                          LOAD * FROM [lib://data]
+                          (txt, header is 1 lines, embedded labels, utf8);`;
+    await app.setScript(loadScript);
+    await app.doReload();
+
+    console.log('Printing the first 10 rows of the Airports table:');
+    // Convert table grid into a string using some functional magic.
     const tableData = await app.getTableData(-1, 10, true, 'Airports');
-
-    // Convert table grid into a string using some functional magic
     const tableDataAsString = tableData.map(row => row.qValue.map(value => value.qText).reduce((left, right) => `${left}\t${right}`)).reduce((row1, row2) => `${row1}\n${row2}`);
-    console.log('First 10 rows of Airports table:');
     console.log(tableDataAsString);
 
-    // Removing data source endpoint from file-connectivity-service
+    console.log('Removing Dropbox connection from file-connectivity-service');
     await request.delete(localhostAccessibleURL);
-
     await session.close();
-
-    process.exit(0);
+    console.log('Engine session closed');
   } catch (err) {
-    console.log(err);
+    console.log('Whoops! An error occurred', err);
+    process.exit(1);
   }
-}
-
-e2e();
+})();
